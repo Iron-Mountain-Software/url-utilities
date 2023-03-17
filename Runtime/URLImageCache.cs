c8793761e94e7c4942ba9e9a8f1e7f67dceb5c81
Chris Monteferrante <chrismonteferrante@Chriss-MacBook-Pro-2.local>
2023-03-17 18:53:10 -0400

    bug fixes

--- Runtime/URLImageCache.cs
+++ Runtime/URLImageCache.cs
@@ -9,17 +9,20 @@ namespace SpellBoundAR.URLUtilities
 
         public static void AddToCache(string url, Sprite sprite)
         {
+            if (string.IsNullOrWhiteSpace(url)) return;
             if (Cache.ContainsKey(url)) Cache[url] = sprite;
-            Cache.Add(url, sprite);
+            else Cache.Add(url, sprite);
         }
 
         public static Sprite LoadFromCache(string url)
         {
+            if (string.IsNullOrWhiteSpace(url)) return null;
             return Cache.ContainsKey(url) ? Cache[url] : null;
         }
 
         public static void RemoveFromCache(string url)
         {
+            if (string.IsNullOrWhiteSpace(url)) return;
             if (Cache.ContainsKey(url)) Cache.Remove(url);
         }
     }
